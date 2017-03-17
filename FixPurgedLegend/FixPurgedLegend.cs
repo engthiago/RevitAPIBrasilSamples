611bab152cbae446f84e0b5b1e16b83106c4eb49
Magson Leone dos Santos <magsonleone@gmail.com>
2017-03-17 18:03:49 -0400

    Update FixPurgedLegend.cs

--- FixPurgedLegend/FixPurgedLegend.cs
+++ FixPurgedLegend/FixPurgedLegend.cs
@@ -14,28 +14,28 @@ namespace FixPurgedLegend
 
         public Result Execute(ExternalCommandData commandData, ref string message, Autodesk.Revit.DB.ElementSet elements)
         {
-            try 
-			{
-				UIApplication uiapp = commandData.Application;
-				Document doc_source = uiapp.ActiveUIDocument.Document;
-				Document doc_destation = uiapp.Application.Documents.Cast<Document>().First(a=>a.Title!=doc_source.Title);
-				CopyPasteOptions op = new CopyPasteOptions();
-				FilteredElementCollector col = new FilteredElementCollector(doc_source);
-				col = col.WhereElementIsNotElementType().OfClass(typeof(View));
-				ElementId id = col.First(a=> ((View)a).ViewType == ViewType.Legend).Id;
-				ICollection<ElementId> list_ids = new List<ElementId>();
-				list_ids.Add(id);
-				Transaction t = new Transaction(doc_destation, "FixPurgedLegend");
-				t.Start();
-				ElementTransformUtils.CopyElements(doc_source,list_ids, doc_destation, null,op);
-				t.Commit();
-                t.Dispose();
-			} 
-			catch (Exception ex)
-            {
-                message = ex.Message + "\n" + ex.StackTrace;
-                return Result.Failed;
-            }
+		try 
+		{
+			UIApplication uiapp = commandData.Application;
+			Document doc_source = uiapp.ActiveUIDocument.Document;
+			Document doc_destation = uiapp.Application.Documents.Cast<Document>().First(a=>a.Title!=doc_source.Title);
+			CopyPasteOptions op = new CopyPasteOptions();
+			FilteredElementCollector col = new FilteredElementCollector(doc_source);
+			col = col.WhereElementIsNotElementType().OfClass(typeof(View));
+			ElementId id = col.First(a=> ((View)a).ViewType == ViewType.Legend).Id;
+			ICollection<ElementId> list_ids = new List<ElementId>();
+			list_ids.Add(id);
+			Transaction t = new Transaction(doc_destation, "FixPurgedLegend");
+			t.Start();
+			ElementTransformUtils.CopyElements(doc_source,list_ids, doc_destation, null,op);
+			t.Commit();
+			t.Dispose();
+		} 
+		catch (Exception ex)
+		{
+		message = ex.Message + "\n" + ex.StackTrace;
+		return Result.Failed;
+		}
 
             return Result.Succeeded;
         }
