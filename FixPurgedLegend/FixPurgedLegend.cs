0bac80ba7c6abb65dbc0cd319e14800f6d095208
Magson Leone dos Santos <magsonleone@gmail.com>
2017-03-17 17:46:07 -0400

    Create FixPurgedLegend.cs

---
+++ FixPurgedLegend/FixPurgedLegend.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+
+namespace FixPurgedLegend
+{
+    [Transaction(TransactionMode.Manual)]
+    internal class FixPurgedLegend : IExternalCommand
+    {
+
+        public Result Execute(ExternalCommandData commandData, ref string message, Autodesk.Revit.DB.ElementSet elements)
+        {
+            try 
+			{
+				Document doc_source = app.ActiveUIDocument.Document;
+				Document doc_destiny = app.Application.Documents.Cast<Document>().First(a=>a.Title!=doc_source.Title);
+				CopyPasteOptions op = new CopyPasteOptions();
+				FilteredElementCollector col = new FilteredElementCollector(doc_source);
+				col = col.WhereElementIsNotElementType().OfClass(typeof(View));
+				ElementId id = col.First(a=> ((View)a).ViewType == ViewType.Legend).Id;
+				ICollection<ElementId> list_ids = new List<ElementId>();
+				list_ids.Add(id);
+				Transaction t = new Transaction(doc_destiny,"FixPurgedLegend");
+				t.Start();
+				ElementTransformUtils.CopyElements(doc_source,list_ids,doc_destiny,null,op);
+				t.Commit();
+			} 
+			catch{}
+        }
+    }
+}
